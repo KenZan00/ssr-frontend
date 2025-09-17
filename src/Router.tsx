d442439608894526f132ef34e11bceba31edd8ea
Kenny Åkerup <keak23@student.bth.se>
2025-09-17 12:11:56 +0200

    Add route form for formtesting

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import TestView from "./views/TestView.tsx";
+import AppForm from "./components/form.tsx";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<TestView />} />
+      <Route path="/form" element={<AppForm />} />
     </Routes>
   );
 }
