ad8a2a57ee39af79bc2b317ca3589ac3d60b5d14
Kenny Åkerup <keak23@student.bth.se>
2025-09-24 09:09:41 +0200

    Adapted for live code from backend

--- src/components/DocumentsList.tsx
+++ src/components/DocumentsList.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
-import { testData } from "../data/testData";
+// import { testData } from "../data/testData";
+import documents from "../models/documents.js";
+
+const docs = await documents.getAllDocuments();
 
 export default function DocumentsList() {
     return (
     <div className="documentsList">
-        {testData.map((doc) => (
-        <p key={doc.id}> <Link to={`/${doc.id}`}>{doc.title}</Link></p>
+        {docs.map((doc) => (
+        <p key={doc._id}> <Link to={`/${doc._id}`}>{doc.title}</Link></p>
         ))}
     </div>
     );
