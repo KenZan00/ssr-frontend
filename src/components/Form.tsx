2ce21275af748ad94d55d6dbf6bf7454c3c9127a
Björn Horndahl <bjorn9111@gmail.com>
2025-09-25 22:34:27 +0200

    Add support for updating or adding a new document

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,50 @@
 import type React from "react";
+import documents from "../models/documents.ts";
+
 // import { testData } from "../data/testData";
 
 
-export default function AppForm({ onSubmit, currentDoc, }: {
-    onSubmit?: (data: {title: string; content: string}) => void;
+export default function AppForm({ currentDoc, }: {
+    // onSubmit?: (data: {title: string; content: string}) => void;
     currentDoc?: {id: string; title: string; content: string}
-}) {
+    }) {
 
-    const submitHandling = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const title = formData.get('title') as string;
-        const content = formData.get('content') as string;
+        const submitHandling = async (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
 
-        onSubmit?.({ title, content });
-        console.log({ title, content });
+            const nativeEvent = event.nativeEvent as SubmitEvent;
+            const submitter = nativeEvent.submitter as HTMLButtonElement | null;
+            const formData = new FormData(event.currentTarget);
+            const title = formData.get('title') as string;
+            const content = formData.get('content') as string;
+            const action = submitter?.value as string;
+            let id = formData.get('id') as string;
+            if (action === 'Lägg till') {
+                id = '';
+            }
+            const inputs = { title, content, action, id };
+
+            try {
+              const savedDoc = await documents.postDocument(inputs);
+              console.log("Document saved:", savedDoc);
+            //   onSubmit?.({ title, content });
+            //   console.log( title, content );
+            } catch (error) {
+              console.log("Failed to save document:", error);
+            }
+        }
+
+        return (
+            <form onSubmit={submitHandling} className="new-doc">
+                <label htmlFor="title">Titel: </label>
+                <input id="title"name="title" type="text" placeholder="Title goes here" defaultValue={currentDoc?.title || ""} />
+
+                <label htmlFor="content">Innehåll: </label>
+                <textarea id="content" name="content" placeholder="Content goes here" defaultValue={currentDoc?.content || ""} />
+
+                <button type="submit" name="action" value="Lägg till">Lägg till</button>
+                <button type="submit" name="action" value="Uppdatera">Uppdatera</button>
+                <input type="hidden" name="id" value={currentDoc?.id || ""}/>
+            </form>
+        )
     }
-
-    return (
-        <form onSubmit={submitHandling} className="new-doc">
-            <label htmlFor="title">Titel: </label>
-            <input id="title"name="title" type="text" placeholder="Title goes here" defaultValue={currentDoc?.title || ""} />
-
-            <label htmlFor="content">Text: </label>
-            <textarea id="content" name="content" placeholder="Content goes here" defaultValue={currentDoc?.content || ""} />
-
-            <button type="submit" name="action" value="Lägg till">Lägg till</button>
-            <button type="submit" name="action" value="Uppdatera">Uppdatera</button>
-            <input type="hidden" name="id" value={currentDoc?.id || ""}/>
-        </form>
-    )
-}
--- src/models/documents.ts
+++ src/models/documents.ts
@@ -1,4 +1,4 @@
-import { baseURL } from "../Utils.tsx";
+import { baseURL } from "../utils/utils.ts";
 
 const documents = {
     getAllDocuments: async function getAllDocuments() {
@@ -10,7 +10,21 @@ const documents = {
         const result = await response.json();
         console.log("Documents:", result.data);
         return result.data;
-    }
+    },
+    postDocument: async function postDocument(data: 
+        { title: string; content: string; action: string; id?: string; }) {
+
+        const response = await fetch(`${baseURL}/`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        console.log("Result of postDocument:", result);
+        return result.data;
+    },
+
 }
 
 export default documents
