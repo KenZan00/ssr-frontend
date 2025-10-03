b26a7c7bb082d918a5d525dc79a11f7b25dbd7d0
Björn Horndahl <bjorn9111@gmail.com>
2025-10-03 20:12:44 +0200

    Rewrite to use post and patch, following better restAPI practice. Also add some redirecting

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,43 +1,46 @@
 import type React from "react";
+import { useNavigate } from "react-router-dom";
 import documents from "../models/documents.ts";
 
-// import { testData } from "../data/testData";
-
-
 export default function AppForm({ currentDoc, }: {
-    // onSubmit?: (data: {title: string; content: string}) => void;
     currentDoc?: {id: string; title: string; content: string}
     }) {
-
+        const navigate = useNavigate();
         const submitHandling = async (event: React.FormEvent<HTMLFormElement>) => {
-            event.preventDefault();
+        event.preventDefault();
+        const nativeEvent = event.nativeEvent as SubmitEvent;
+        const submitter = nativeEvent.submitter as HTMLButtonElement | null;
+        const formData = new FormData(event.currentTarget);
+        const title = formData.get('title') as string;
+        const content = formData.get('content') as string;
+        const action = submitter?.value as string;
+        const id = currentDoc?.id as string;
 
-            const nativeEvent = event.nativeEvent as SubmitEvent;
-            const submitter = nativeEvent.submitter as HTMLButtonElement | null;
-            const formData = new FormData(event.currentTarget);
-            const title = formData.get('title') as string;
-            const content = formData.get('content') as string;
-            const action = submitter?.value as string;
-            let id = formData.get('id') as string;
-            if (action === 'Lägg till') {
-                id = '';
-            }
-            const inputs = { title, content, action, id };
-
+        if (action === 'Lägg till') {
             try {
-              const savedDoc = await documents.postDocument(inputs);
-              console.log("Document saved:", savedDoc);
-            //   onSubmit?.({ title, content });
-            //   console.log( title, content );
+                const newDoc = await documents.postDocument({ title, content });
+                navigate("/");
+                console.log("Document created:", newDoc);
             } catch (error) {
-              console.log("Failed to save document:", error);
+                console.log("Failed to save document:", error);
             }
+        }
+
+        if (action === 'Uppdatera') {
+            try {
+                const savedDoc = await documents.patchDocument({ title, content, id });
+                alert('Updated')
+                console.log("Document saved:", savedDoc);
+            } catch (error) {
+                console.log("Failed to save document:", error);
+            }
+        }
         }
 
         return (
             <form onSubmit={submitHandling} className="new-doc">
                 <label htmlFor="title">Titel: </label>
-                <input id="title"name="title" type="text" placeholder="Title goes here" defaultValue={currentDoc?.title || ""} />
+                <input id="title" name="title" type="text" placeholder="Title goes here" defaultValue={currentDoc?.title || ""} />
 
                 <label htmlFor="content">Innehåll: </label>
                 <textarea id="content" name="content" placeholder="Content goes here" defaultValue={currentDoc?.content || ""} />
