086cf82854c215a64e77be73859cd6b089276f75
Björn Horndahl <bjorn9111@gmail.com>
2025-10-03 20:15:29 +0200

    Add support for localStorage

--- src/models/documents.ts
+++ src/models/documents.ts
@@ -2,9 +2,19 @@ import { baseURL } from "../utils/utils.ts";
 
 const documents = {
     getAllDocuments: async function getAllDocuments() {
-
-        const response = await fetch(`${baseURL}/`, {
-            method: 'GET'
+        // const token = localStorage.getItem("token");
+        // // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthbGxlQGdtYWlsLmNvbSIsImlhdCI6MTc1OTI2NTI0NywiZXhwIjoxNzU5ODcwMDQ3fQ.v6PSdOVAI2D4iCXNsrNv_ltRS5MrN2GIJEgrVmMl4Ak"
+        
+        // if (!token) {
+        //     const navigate = useNavigate();
+        //     navigate("/login");
+        // }
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${baseURL}/documents`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            }
         });
 
         const result = await response.json();
@@ -12,11 +22,45 @@ const documents = {
         return result.data;
     },
     postDocument: async function postDocument(data: 
-        { title: string; content: string; action: string; id?: string; }) {
+        { title: string; content: string; }) {
+        // const token = localStorage.getItem("token");
+        // // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthbGxlQGdtYWlsLmNvbSIsImlhdCI6MTc1OTI2NTI0NywiZXhwIjoxNzU5ODcwMDQ3fQ.v6PSdOVAI2D4iCXNsrNv_ltRS5MrN2GIJEgrVmMl4Ak"
 
-        const response = await fetch(`${baseURL}/`, {
+        // if (!token) {
+        //     const navigate = useNavigate();
+        //     navigate("/login");
+        // }
+        const token = localStorage.getItem("token");
+        const response = await fetch(`${baseURL}/documents`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        console.log("Result of postDocument:", result);
+        return result.data;
+    },
+    patchDocument: async function patchDocument(data: 
+        { title: string; content: string; id: string; }) {
+        // const token = localStorage.getItem("token");
+        // // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImthbGxlQGdtYWlsLmNvbSIsImlhdCI6MTc1OTI2NTI0NywiZXhwIjoxNzU5ODcwMDQ3fQ.v6PSdOVAI2D4iCXNsrNv_ltRS5MrN2GIJEgrVmMl4Ak"
+
+        // if (!token) {
+        //     const navigate = useNavigate();
+        //     navigate("/login");
+        // }
+        const token = localStorage.getItem("token");
+        const id = data.id;
+        const response = await fetch(`${baseURL}/documents/${id}`, {
+            method: 'PATCH',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
             body: JSON.stringify(data)
         });
 
