8b38766fad1b672ce12e0c4d42731b87a5301e1d
Kenny Åkerup <keak23@student.bth.se>
2025-10-14 15:51:48 +0200

    Functioning (hard-coded) socket for codemode client.

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -3,28 +3,58 @@ import { io } from "socket.io-client";
 import Editor from "@monaco-editor/react";
 import documents from "../models/documents.ts";
 
-export default function CodeMode({ currentDoc }) {
+const SERVER_URL = "http://localhost:3000";
 
-    const [editorValue, setEditorValue] = useState('')
+export default function CodeMode({ currentDoc, }: {
+    currentDoc?: {id: string; title: string; content: string}
+    }) {
+    
+        const [editorContent, setEditorContent] = useState('')
 
-    function handleEditorChange(value) {
-        console.log('here is the current model value:', value);
-        setEditorValue(value)
-    }
+        const socket = useRef(null);
 
-    return (
-    <Editor
-    className="code-editor"
-    height="90vh"
+        useEffect(() => {
+            socket.current = io(SERVER_URL);
 
-    theme="vs-dark"
-    defaultLanguage="javascript"
-    defaultValue="// some comment"
-    value={editorValue}
-    onChange={handleEditorChange}
-    options={{
-        fontSize: 20,
-    }}
-    />
+            // if(currentDoc?.id) {
+            socket.current.emit('create', "1")
+            // }
+
+            socket.current.on('code', (data) => {
+                // setTitle(data.title);
+                setEditorContent(data.editorContent)
+            });
+
+            return () => {
+                socket.current.disconnect();
+            }
+        }, [currentDoc?.id]);
+
+        function handleEditorChange(content: string | undefined) {
+            // console.log('here is the current model value:', value);
+            const value = content ?? "";
+            setEditorContent(value);
+
+            socket.current.emit("code", {
+                _id: "1",
+                title: 'itel1',
+                editorContent: value
+            });
+        }
+
+        return (
+        <Editor
+        className="code-editor"
+        height="90vh"
+        width="100%"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        value={editorContent}
+        onChange={handleEditorChange}
+        options={{
+            fontSize: 20,
+        }}
+        />
     )
 }
