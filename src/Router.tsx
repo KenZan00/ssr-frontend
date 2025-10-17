bf82be19d0960fc150ca5e10c5878b8a9bb7b6f1
Kenny Åkerup <keak23@student.bth.se>
2025-10-17 10:22:47 +0200

    Separated pages in router to be able to use different styles or layout in the page

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import AppFormView from "./views/AppFormView.tsx";
 import DocumentsListView from "./views/DocumentsListView.tsx";
 import CodeModeView from "./views/CodeModeView.tsx";
+import HeroView from "./views/HeroView.tsx";
+import Layout from "./layout/layout.tsx";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<DocumentsListView />} />
-      <Route path="/:id" element={<AppFormView />} />
-      <Route path="/code" element={<CodeModeView />} />
+      <Route path="/hero" element={<HeroView />} />
+
+      <Route element={<Layout />}>
+        <Route path="/" element={<DocumentsListView />} />
+        <Route path="/:id" element={<AppFormView />} />
+        <Route path="/code" element={<CodeModeView />} />
+        <Route path="/hero" element={<HeroView />} />
+      </Route>
     </Routes>
   );
 }
