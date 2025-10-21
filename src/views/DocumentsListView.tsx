3d94cd62c9fe9f858068a27273aec66645971985
Björn Horndahl <bjorn9111@gmail.com>
2025-10-21 03:36:34 +0200

    Add <LoginForm /> if invalid token

--- src/views/DocumentsListView.tsx
+++ src/views/DocumentsListView.tsx
@@ -1,6 +1,20 @@
 import DocumentsList from "../components/DocumentsList"
+import LoginForm from "../components/LoginForm";
+import { Link } from "react-router-dom";
+import auth from "../models/auth.ts";
+const validToken = await auth.token();
+
+
 
 export default function DocumentsListView() {
+    if (!validToken) {
+        return <div className="login-form">
+            <h3><Link to={`/signup`}>Klicka här för att registrera dig</Link></h3>
+            <h2>Login</h2>
+            <LoginForm />
+        </div>
+    }
+
     return (
         <>
             <h2>Dokument</h2>
