5d3a93c33151014ac65e9750d7f6bf6827ab0db8
Björn Horndahl <bjorn9111@gmail.com>
2025-10-24 19:31:31 +0200

    Add protected and logout routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import Protected from "./utils/Protected.tsx";
 // import TestView from "./views/TestView.tsx";
 import AppFormView from "./views/AppFormView.tsx";
 import DocumentsListView from "./views/DocumentsListView.tsx";
 import LoginFormView from "./views/LoginFormView.tsx";
 import SignupFormView from "./views/SignupFormView.tsx";
+import LogoutView from "./views/LogoutView.tsx";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<DocumentsListView />} />
-      <Route path="/:id" element={<AppFormView />} />
+      <Route element={<Protected />}>
+        <Route path="/" element={<DocumentsListView />} />
+        <Route path="/:id" element={<AppFormView />} />
+      </Route>
       <Route path="/login" element={<LoginFormView />} />
       <Route path="/signup" element={<SignupFormView />} />
+      <Route path="/logout" element={<LogoutView />} />
     </Routes>
   );
 }
