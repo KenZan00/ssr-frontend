a40bd306389fdf0ae5ac553653db912b165e0510
Kenny Åkerup <keak23@student.bth.se>
2025-10-27 16:48:04 +0100

    Updates for creating new document

--- src/views/DocumentsListView.tsx
+++ src/views/DocumentsListView.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import DocumentsList from "../components/DocumentsList"
 
 export default function DocumentsListView() {
     return (
         <>
             <h2>Dokument</h2>
+            <Link to="/newdoc">New Document</Link>
             <DocumentsList />
         </>
     );
