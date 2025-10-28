65a5f2cf426e9f405cda170bab961d06e8205303
Kenny Åkerup <keak23@student.bth.se>
2025-10-28 08:55:50 +0100

    Added type-based routing thru link

--- src/components/DocumentsList.tsx
+++ src/components/DocumentsList.tsx
@@ -9,12 +9,17 @@ export default function DocumentsList() {
         <div className="documentsList">
         
         <h3>Mina egna dokument:</h3>
-        {docs.owner.map((doc: { _id: string; title: string; content: string; created_at: string }) => (
-            <p key={doc._id}> <Link to={`/${doc._id}`} state={{ doc }}>{doc.title}</Link></p>
+        {docs.owner.map((doc: { _id: string; title: string; content: string; created_at: string; type: string }) => (
+            <p key={doc._id}>
+                <Link to={doc.type === 'code' ? `/code/${doc._id}` :`/${doc._id}`}
+                state={{ doc }}>{doc.title}</Link>
+            </p>
         ))}
         <h3>Dokument skapad av andra jag får uppdatera:</h3>
-        {docs.editor.map((doc: { _id: string; title: string; content: string; created_at: string }) => (
-            <p key={doc._id}> <Link to={`/${doc._id}`} state={{ doc }}>{doc.title}</Link></p>
+        {docs.editor.map((doc: { _id: string; title: string; content: string; created_at: string; type: string }) => (
+            <p key={doc._id}>
+                <Link to={doc.type === 'code' ? `/code/${doc._id}` : `/${doc._id}`}
+                state={{ doc }}>{doc.title}</Link></p>
         ))}
     </div>
     );
