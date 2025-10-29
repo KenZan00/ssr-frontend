0fa538a171b180a81670807beb88b86ad6f46fea
Kenny Åkerup <keak23@student.bth.se>
2025-10-29 13:10:16 +0100

    Added classname for code-editor styling

--- src/views/CodeModeView.tsx
+++ src/views/CodeModeView.tsx
@@ -8,9 +8,7 @@ export default function CodeModeView() {
     return (
         <div className="code-editor-view">
         <h2>Code Mode</h2>
-            <div className="code-editor-container">
                 <CodeMode currentDoc={doc && { id: doc._id, title: doc.title, content: doc.content }} />
-            </div>
         </div>
     );
 }
