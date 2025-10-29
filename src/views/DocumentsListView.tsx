1c92330fb35f54cafabb1a46d8e21dcf58469721
Kenny Åkerup <keak23@student.bth.se>
2025-10-29 13:10:39 +0100

    Updated links with link for new code-document

--- src/views/DocumentsListView.tsx
+++ src/views/DocumentsListView.tsx
@@ -5,7 +5,10 @@ export default function DocumentsListView() {
     return (
         <>
             <h2>Dokument</h2>
-            <Link to="/newdoc">New Document</Link>
+                <div className="new-doc-links">
+                    <Link to="/newdoc" className="sign-up">Nytt Text Document</Link>
+                    <Link to="/code" className="sign-up">Nytt Code Document</Link>
+                </div>
             <DocumentsList />
         </>
     );
