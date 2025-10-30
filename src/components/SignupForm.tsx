65eea1cb1d40adee98a3ad1168c20a0d6589359e
Björn Horndahl <bjorn9111@gmail.com>
2025-10-30 18:38:55 +0100

    Navigate if no errors

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -17,11 +17,11 @@ export default function SignupForm() {
         try {
             const signupResult = await auth.signup({ email, password });
             console.log("Signup complete:", signupResult);
-            if (signupResult?.data?.type === 'success') {
-                navigate("/");
-            }
             if (signupResult?.errors?.[0]?.status === 401) {
                 alert(signupResult.errors[0].detail);
+            }
+            if (signupResult) {
+                navigate("/");
             }
         } catch (error) {
             console.log("Failed to signup:", error);
