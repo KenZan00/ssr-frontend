1bb68033f4f3735ee9f334771568936092c89558
Kenny Åkerup <keak23@student.bth.se>
2025-10-31 10:08:45 +0100

    Changed to working with azure database

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 // Azure connection base url
 
-// const baseURL = 'https://jsramverk-keak23-fkarewcehug7hsde.northeurope-01.azurewebsites.net'
-const baseURL = 'http://localhost:3000'
+const baseURL = 'https://jsramverk-keak23-fkarewcehug7hsde.northeurope-01.azurewebsites.net'
+// const baseURL = 'http://localhost:3000'
 
 
 export { baseURL }
