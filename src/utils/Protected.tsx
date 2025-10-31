830f3f473e6a55a3b0e1f4eb667b5f8a2b469c54
Björn Horndahl <bjorn9111@gmail.com>
2025-10-31 20:29:02 +0100

    Fix login flow

--- src/utils/Protected.tsx
+++ src/utils/Protected.tsx
@@ -1,8 +1,25 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import auth from "../models/auth.ts";
-const validToken = await auth.token();
 
 const Protected = () => {
+    const [validToken, setValidToken] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        const authTokenState = async () => {
+            try {
+                const getTokenAuth = await auth.token();
+                setValidToken(getTokenAuth);
+            } catch (error) {
+                console.error("Failed to auth token", error);
+            }
+        };
+
+        authTokenState();
+    }, []);
+    if (validToken === null) {
+        return <div></div>;
+    }
     return validToken ? <Outlet></Outlet> : <Navigate to='/login'></Navigate>
 }
 
