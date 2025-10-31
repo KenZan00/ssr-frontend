1fe14308c9da076de4b2463d78685ea554463c2d
Kenny Åkerup <keak23@student.bth.se>
2025-10-31 10:41:05 +0100

    Test route not found

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ssr-frontend',
+  base: '/ssr-frontend/',
   preview: { port: 5173 }
 })
